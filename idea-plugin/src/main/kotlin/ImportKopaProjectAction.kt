de141bb956159fde07dfef9b3c009662e2d57b91
Kevin Fritz <kevinfritz210@gmail.com>
2022-02-06 12:43:52 +0100

    Update idea plugin to make import functional. Still hacky though

--- idea-plugin/src/main/kotlin/ImportKopaProjectAction.kt
+++ idea-plugin/src/main/kotlin/ImportKopaProjectAction.kt
@@ -1,13 +1,17 @@
-
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ModuleRootModificationUtil
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.JarFileSystem
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.util.io.URLUtil
 import io.kharf.kopa.packages.FileManifestInterpreter
 import java.io.File
 
@@ -28,12 +32,29 @@ class ImportKopaProjectAction : AnAction() {
         val interpretation = fileInterpreter.interpret(kopaToml!!)
         val homeDir = System.getProperty("user.home")
         val app = ApplicationManager.getApplication()
+        val projectLibraryTable = LibraryTablesRegistrar.getInstance().getLibraryTable(project)
+        val projectLibraryModel = projectLibraryTable.modifiableModel
         interpretation.dependencies.forEach {
-            app.runWriteAction {
-                ModuleRootModificationUtil.addModuleLibrary(
-                    module!!,
-                    "jar://$homeDir/.kopa/packages/${it.name}-${it.version}.jar!/"
-                )
+            val kopaDepName = "Kopa: ${it.jarName}"
+            val libExists = projectLibraryModel.libraries.find { it.name == kopaDepName } != null
+            if (!libExists) {
+                val lib = projectLibraryModel.createLibrary(kopaDepName)
+                val pathUrl: String =
+                    VirtualFileManager.constructUrl(
+                        URLUtil.JAR_PROTOCOL,
+                        "$homeDir/.kopa/packages/${it.jarName}"
+                    ) + JarFileSystem.JAR_SEPARATOR
+                val file: VirtualFile = VirtualFileManager.getInstance().findFileByUrl(pathUrl)!!
+                val libModifiableModel = lib.modifiableModel
+                libModifiableModel.addRoot(file, OrderRootType.CLASSES)
+                app.runWriteAction {
+                    libModifiableModel.commit()
+                    projectLibraryModel.commit()
+                    ModuleRootModificationUtil.addDependency(
+                        module!!,
+                        lib
+                    )
+                }
             }
         }
         val modifiableModel = ModuleRootManager.getInstance(module!!).modifiableModel
